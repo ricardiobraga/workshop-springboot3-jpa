4252f949a32443b5566cc5559b94941a26b3679c
ricardiobraga <rcardiobraga@gmail.com>
2023-01-24 18:43:43 -0300

    Project created

---
+++ src/main/java/com/projetoweb/projetowebspringboot/ProjetowebSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.projetoweb.projetowebspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetowebSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetowebSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoweb/projetowebspringboot/ProjetowebSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoweb.projetowebspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetowebSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
