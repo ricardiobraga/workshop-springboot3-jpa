65be89f92d30e4634fc359a112e12e331d31288c
ricardiobraga <rcardiobraga@gmail.com>
2023-02-03 16:04:31 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/projetoweb/projetowebspringboot/repositories/UserRepository.java
+++ src/main/java/com/projetoweb/projetowebspringboot/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.projetoweb.projetowebspringboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.projetoweb.projetowebspringboot.entities.User;
 
+// @Repository - nesse caso a annotation é opcional pq a interface já está herdando o "JpaRepository"
 public interface UserRepository extends JpaRepository<User, Long> {
     
 }
