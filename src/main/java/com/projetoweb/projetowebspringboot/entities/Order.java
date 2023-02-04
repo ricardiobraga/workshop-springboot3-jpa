6feaa002cafaad86e8bba86097e6974ed7f43aed
ricardiobraga <rcardiobraga@gmail.com>
2023-02-03 22:38:18 -0300

    OrderStatus enum

--- src/main/java/com/projetoweb/projetowebspringboot/entities/Order.java
+++ src/main/java/com/projetoweb/projetowebspringboot/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import com.projetoweb.projetowebspringboot.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -26,6 +27,8 @@ public class Order implements Serializable {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+
+    private Integer orderStatus;
     
     @ManyToOne
     @JoinColumn(name = "client_Id")
@@ -36,9 +39,10 @@ public class Order implements Serializable {
         
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,  OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -64,6 +68,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }    
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){ 
+        this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
